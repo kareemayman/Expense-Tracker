543fb16bd55c723cd443734e347f11fd31ad09ad
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-11-09 20:47:26 +0200

    Added Logic To Add Entries

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('button')
 const textInput = document.querySelector('input[id="text"]')
 const amountInput = document.querySelector('input[id="amount"]')
+const entries = document.querySelector('.history .entries')
+
+let balance = 0
 
 
 // Event Listener For Clicking On Add Transaction Button
@@ -8,7 +11,26 @@ button.addEventListener('click', e => {
 
     // Check If Text Or Amount Inputs Empty
     if(!checkIfInputEmpty()) {
+
+        // Add To Balance
+        balance += amountInput.value
+        balance = Number(balance).toFixed(2)
+
+        // Create A New Entry
+        let div = document.createElement('div')
+        if (balance >= 0) div.className = 'entry income'
+        else div.className = 'entry expense'
         
+        // Add Text, Amount To Entry
+        div.innerHTML = `<span class="entry-name">${textInput.value}</span>
+                    <span class="amount">${amountInput.value >= 0 ? `+${amountInput.value}` : amountInput.value}</span>`
+
+        // Append Entry To Entries Div
+        entries.append(div)
+
+        // Clear Inputs
+        amountInput.value = ""
+        textInput.value = ""
     }
 })
 
